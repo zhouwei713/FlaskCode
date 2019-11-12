d93dee27e07aa62dc127eca896e834c4b9932e48
zhouwei713 <736552883@qq.com>
2019-11-12 10:59:00 +0800

    2a

--- HelloFlask/app.py
+++ HelloFlask/app.py
@@ -5,9 +5,51 @@
 @File: app.py
 """
 
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, session, redirect
+from urllib.parse import urlparse
 
 app = Flask(__name__)
+app.secret_key = 'Very Hard Secret'
+
+
+def check_next(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(target)
+    return ref_url.netloc == test_url.netloc
+
+
+@app.route('/login/')
+def login():
+    session['loginID'] = 'admin'
+    target = request.args.get('next')
+    if check_next(target):
+        return redirect(target)
+    return redirect(url_for('hello'))
+
+
+@app.route('/needlogin1/')
+def needLogin1():
+    if 'loginID' in session:
+        return '<h1>Hello, needLogin1!</h1>'
+    else:
+        return """
+            <h1>Login</h1><a href="%s">Go To Login</a>
+                """ % url_for('login', next=request.url)
+
+
+@app.route('/needlogin2/')
+def needLogin2():
+    if 'loginID' in session:
+        return '<h1>Hello, needLogin2!</h1>'
+    else:
+        return redirect(url_for('loginPage', next=request.full_path))
+
+
+@app.route('/logout/')
+def logout():
+    if 'loginID' in session:
+        session.pop('loginID')
+    return redirect(url_for('welcome'))
 
 
 @app.route('/hello')
@@ -19,8 +61,12 @@ def hello():
 @app.route('/user/', defaults={'name': '陌生人'})
 @app.route('/user/<name>')
 def welcome(name):
-    print(url_for('hello'))
-    return '<h1>Hello, %s!</h1>' % name
+    res = '<h1>Hello, %s!</h1>' % name
+    if 'loginID' in session:
+        res += 'Authenticated'
+    else:
+        res += 'UnAuthenticated'
+    return res
 
 
 @app.route('/test/')
